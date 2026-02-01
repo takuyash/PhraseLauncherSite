0155d6f38f14a71dd811b48302a233b291cdc9eb
takuya <14888534+takuyash@users.noreply.github.com>
2026-02-01 17:28:54 +0900

    英語サイトの作成

---
+++ en/js/info-data-en.js
@@ -0,0 +1,12 @@
+var infos = [
+  {
+    date: "2026-02-01",
+    title: "v0.4.0 Released",
+    body: "Added Multilingual (English) support."
+  },
+  {
+    date: "2026-01-25",
+    title: "Official Website Launched",
+    body: "The official website for PhraseLauncher is now open. Usage guides and update information will be posted here."
+  }
+];
