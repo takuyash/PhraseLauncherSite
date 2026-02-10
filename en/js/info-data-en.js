29ccd73ac66f31d1947d0b7582f7f3e327233cb6
takuya <14888534+takuyash@users.noreply.github.com>
2026-02-10 22:25:03 +0900

    リリース情報の追加

--- en/js/info-data-en.js
+++ en/js/info-data-en.js
@@ -1,11 +1,20 @@
 var infos = [
   {
+    date: "2026-02-10",
+    title: "v0.6.0 Released",
+    body: "・ Fixed so that predefined phrases can be moved by selecting multiple rows.<br>・ Added an option to choose whether to include notes in search results.<br>・Added an encryption feature for predefined phrases.<br>・ Updated settings to allow turning the predefined phrase encryption feature on and off.<br>・Added import and export features for predefined phrases.",
+    link: {
+      url: "https://github.com/takuyash/SimpleFolderLauncher/releases",
+      label: "View GitHub Releases"
+    }
+  },
+  {
     date: "2026-02-08",
     title: "v0.5.0 Released",
     body: "・Added a setting to enable or disable hotkey activation.<br>・Added the ability to change the hotkey type and the number of required key presses.",
     link: {
       url: "https://github.com/takuyash/SimpleFolderLauncher/releases",
-      label: "GitHub Releases を見る"
+      label: "View GitHub Releases"
     }
   },
   {
